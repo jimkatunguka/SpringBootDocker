630c1c952c8d2b8254a5853035ab165709c6cf71
jimkatunguka <jimkatunguka@gmail.com>
2020-05-06 10:23:05 -0500

    first commit

---
+++ src/test/java/jim/katunguka/Spring/BootDocker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package jim.katunguka.Spring.BootDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
