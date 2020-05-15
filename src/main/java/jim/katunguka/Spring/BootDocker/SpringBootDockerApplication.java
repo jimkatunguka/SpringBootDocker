938da0a943ce4fedbe6b166fb8af87c2c67638b3
jimkatunguka <jimkatunguka@gmail.com>
2020-05-15 16:23:29 -0500

    check out

--- src/main/java/jim/katunguka/Spring/BootDocker/SpringBootDockerApplication.java
+++ src/main/java/jim/katunguka/Spring/BootDocker/SpringBootDockerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringBootDockerApplication {
 	
-	@GetMapping("/message")
+	@GetMapping("/")
 	public String getMessage() {
 		return "Welcome to the real world";
 	}
